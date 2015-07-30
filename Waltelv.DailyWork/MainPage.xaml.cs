270f75ed77c07d1204f82dd5685dceea192ca0c8
walterlv <xblero@gmail.com>
2015-07-30 16:39:41 +0800

    添加DailyWork基本页面样式

--- Waltelv.DailyWork/MainPage.xaml.cs
+++ Waltelv.DailyWork/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +16,35 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Waltelv.DailyWork
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            Loading += OnLoading;
+        }
+
+        private void OnLoading(FrameworkElement sender, object e)
+        {
+            Color accent = (Color)Application.Current.Resources["Theme.Color.Accent"];
+            Color accentLight = (Color)Application.Current.Resources["Theme.Color.AccentLight"];
+            Color accentDark = (Color)Application.Current.Resources["Theme.Color.AccentDark"];
+            Color foreground = (Color)Application.Current.Resources["Theme.Color.AccentForeground"];
+
+            ApplicationView view = ApplicationView.GetForCurrentView();
+            view.TitleBar.BackgroundColor = view.TitleBar.InactiveBackgroundColor = accent;
+            view.TitleBar.ButtonBackgroundColor = view.TitleBar.ButtonInactiveBackgroundColor = accent;
+            view.TitleBar.ForegroundColor = view.TitleBar.ButtonForegroundColor = foreground;
+            view.TitleBar.InactiveForegroundColor = view.TitleBar.ButtonInactiveForegroundColor = foreground;
+            view.TitleBar.ButtonHoverBackgroundColor = accentLight;
+            view.TitleBar.ButtonPressedBackgroundColor = accentDark;
+            view.TitleBar.ButtonHoverForegroundColor = view.TitleBar.ButtonPressedForegroundColor = foreground;
+
+            CoreApplicationView coreView = CoreApplication.GetCurrentView();
+            coreView.TitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(TitleBarPanel);
         }
     }
 }
---
+++ Waltelv.DailyWork/Views/DayTimeCellView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Waltelv.DailyWork.Views
+{
+    public sealed partial class DayTimeCellView : UserControl
+    {
+        public DayTimeCellView()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Time
+        {
+            get { return TimeTextBlock.Text; }
+            set { TimeTextBlock.Text = value; }
+        }
+    }
+}
